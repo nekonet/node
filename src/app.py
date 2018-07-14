0ecb059b2900375cb3510d95934ebcba7c2e6e1e
Ahisahar <approdriguez@correo.ugr.es>
2018-07-14 19:03:12 +0200

    test websocket

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, jsonify, request
 
 from Crypto.PublicKey import RSA
-from datetime import import datetime
+from Crypto.Cipher import AES
+from Crypto import Random
+from datetime import datetime
+from random import randint
 
-from diff
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-
+socketio = SocketIO(app)
 
 def validateToken(token):
     with open('private.pem','r') as priv_pem:
@@ -19,15 +22,34 @@ def validateToken(token):
         else:
             return True
 
-
-def establish_diffie_hellman():
-    pass
+@socketio.on('establish-dh')
+def establish_diffie_hellman(data):
+    # Generate id of the transaction
+    # Receive two shared primes
+    # Generate random private secret
+    import pdb; pdb.set_trace()
+    p = request.form.get('p')
+    g = request.form.get('g')
+    node_private_secret = randint(0,100) 
+    # send A = g^a mod p
+    node_shared_node = (g**private_secret) % p
+    # Receive client secret
+    client_shared_secret = request.form.get('s')
+    # Shared secret
+    shared_secret = (client_shared_secret ** node_private_secret) % p
+    
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(shared_secret, AES.MODE_CFB, iv)
+    print(cipher, iv)
+    return (cipher, iv)
 
 @app.route("/enroute", methods={'POST'})
 def enroute():
     token = request.headers.get('authorization', None)
     valid = validateToken(token)
     if valid:
+        pass
+    return ('hola')
         # Establish diffie hellman connection
 
 
@@ -38,3 +60,4 @@ def enroute():
 
 
         # Send message to the next node
+
